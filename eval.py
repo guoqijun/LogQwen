a590f82601709433dabd86967e4300044ed69b56
guoqijun <guoqijun1994@gmail.com>
2025-02-24 15:06:44 +0800

    添加打印

--- eval.py
+++ eval.py
@@ -11,7 +11,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 max_content_len = 100
 max_seq_len = 128
 batch_size = 32
-dataset_name = 'BGL'   # 'Thunderbird' 'HDFS_v1'  'BGL'  'Liberty‘
+dataset_name = 'BGL'  # 'Thunderbird' 'HDFS_v1'  'BGL'  'Liberty‘
 # data_path = r'/mnt/public/gw/SyslogData/{}/test.csv'.format(dataset_name)
 data_path = r'/mnt/workspace/test.csv'
 
@@ -27,11 +27,11 @@ ft_path = os.path.join(ROOT_DIR, r"ft_model_{}".format(dataset_name))
 device = torch.device("cuda:0")
 
 print(
-f'dataset_name: {dataset_name}\n'
-f'batch_size: {batch_size}\n'
-f'max_content_len: {max_content_len}\n'
-f'max_seq_len: {max_seq_len}\n'
-f'device: {device}')
+    f'dataset_name: {dataset_name}\n'
+    f'batch_size: {batch_size}\n'
+    f'max_content_len: {max_content_len}\n'
+    f'max_seq_len: {max_seq_len}\n'
+    f'device: {device}')
 
 
 def evalModel(model, dataset, batch_size):
@@ -50,11 +50,11 @@ def evalModel(model, dataset, batch_size):
             pre = bathc_i
 
             this_batch_seqs, _ = dataset.get_batch(this_batch_indexes)
-            print(this_batch_seqs)
+            print("给model的数据", this_batch_seqs)
             outputs_ids = model(this_batch_seqs)
-            print(outputs_ids)
+            print("推理后：", outputs_ids)
             outputs = model.Llama_tokenizer.batch_decode(outputs_ids)
-            print(outputs)
+            print("编码后：", outputs)
 
             for text in outputs:
                 matches = re.findall(r' (.*?)\.<|end_of_text|>', text)
@@ -64,7 +64,7 @@ def evalModel(model, dataset, batch_size):
                     preds.append('')
 
     preds_copy = np.array(preds)
-    preds = np.zeros_like(preds_copy,dtype=int)
+    preds = np.zeros_like(preds_copy, dtype=int)
     preds[preds_copy == 'anomalous'] = 1
     preds[preds_copy != 'anomalous'] = 0
     gt = dataset.get_label()
@@ -80,7 +80,7 @@ def evalModel(model, dataset, batch_size):
     print(f'Number of anomalous seqs: {num_anomalous}; number of normal seqs: {num_normal}')
 
     pred_num_anomalous = (preds == 1).sum()
-    pred_num_normal =  (preds == 0).sum()
+    pred_num_normal = (preds == 0).sum()
 
     print(
         f'Number of detected anomalous seqs: {pred_num_anomalous}; number of detected normal seqs: {pred_num_normal}')
