930b8c5806d0df7ab8754e31806f0502460dce3f
guoqijun <guoqijun1994@gmail.com>
2025-02-28 16:47:33 +0800

    模型路径

--- use_qwen/use_qwen.py
+++ use_qwen/use_qwen.py
@@ -23,19 +23,39 @@ tokenizer = AutoTokenizer.from_pretrained(Qwen_path, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(Qwen_path, trust_remote_code=True, low_cpu_mem_usage=True,
                                              device_map=device).eval()
 
+
+def eval(input):
+    # 对输入文本进行分词
+    inputs = tokenizer(input, return_tensors='pt').to(model.device)
+
+    # 进行推理
+    with torch.no_grad():
+        outputs = model.generate(**inputs, max_new_tokens=512, do_sample=True, top_p=0.85, temperature=0.35)
+
+    # 将输出的 token 序列转换为文本
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response
+
+
 # 输入文本
 input_text = "介绍一下北京的故宫。"
-
-# 对输入文本进行分词
-inputs = tokenizer(input_text, return_tensors='pt').to(model.device)
-
-# 进行推理
-with torch.no_grad():
-    outputs = model.generate(**inputs, max_new_tokens=512, do_sample=True, top_p=0.85, temperature=0.35)
-
-# 将输出的 token 序列转换为文本
-response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
+response = eval(input_text)
 # 打印生成的回复
-print("输入问题:", input_text)
+print("输入问题:", )
 print("Qwen 模型的回复:", response)
+
+try:
+    # 以只读模式打开 test.log 文件
+    with open(data_path, 'r', encoding='utf-8') as file:
+        # 读取文件的全部内容
+        content = file.read()
+        # 按换行符分割内容成列表
+        lines = content.split('\n')
+except FileNotFoundError:
+    print("未找到 test.log 文件，请检查文件路径是否正确。")
+except Exception as e:
+    print(f"读取文件时出现错误: {e}")
+
+for line in lines:
+    response = eval(line)
+    print(f"模型输出", response)
