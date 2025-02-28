6de48637381811d45225f1b6d36c511f721c4aa3
guoqijun <guoqijun1994@gmail.com>
2025-02-28 16:40:55 +0800

    模型路径

--- use_qwen/use_qwen.py
+++ use_qwen/use_qwen.py
@@ -9,17 +9,19 @@ data_path = os.path.join(ROOT_DIR, '../test.log')
 
 # Bert_path = r"/mnt/workspace/.cache/modelscope/hub/models/AI-ModelScope/bert-base-uncased"
 Qwen_path = r"/mnt/workspace/.cache/modelscope/models/Qwen/Qwen2___5-1___5B-Instruct"
+# 检查是否有可用的GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print(
+    f'dataset_name: {data_path}\n'
+    f'Qwen_path: {Qwen_path}\n'
+    f'device: {device}')
 
 # 加载分词器和模型
 # 这里使用 Qwen-7B-Chat 模型，你可以根据需要更换为其他 Qwen 系列模型
 tokenizer = AutoTokenizer.from_pretrained(Qwen_path, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(Qwen_path, device_map="auto", trust_remote_code=True).eval()
-
-# 检查是否有可用的GPU
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# 将模型部署到指定设备
-model.to(device)
+model = AutoModelForCausalLM.from_pretrained(Qwen_path, trust_remote_code=True, low_cpu_mem_usage=True,
+                                             device_map=device).eval()
 
 # 输入文本
 input_text = "介绍一下北京的故宫。"
